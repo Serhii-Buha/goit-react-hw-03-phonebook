c742ba73b254e019cac67f7f9b1f432a25f24cfe
Serhii-Buha <serhii.buga@gmail.com>
2023-05-08 19:55:15 +0300

    add localStorage: componentDidMount, componentDidUpdate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -55,6 +55,21 @@ export class App extends Component {
     }));
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     const filterContacts = this.getVisibleFilter();
     const { contacts } = this.state;
