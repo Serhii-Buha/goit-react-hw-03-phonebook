471d5e6f4234198dda4880e4a6ea687af92fda94
Serhii-Buha <114286179+Serhii-Buha@users.noreply.github.com>
2023-05-08 17:05:38 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import { Container } from './ContactForm/ContactForm.styled';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  formSubmitHandler = data => {
+    console.log(data);
+
+    const { contacts } = this.state;
+    const isExist = contacts.some(contact => contact.name === data.name);
+
+    if (isExist) {
+      alert(`${data.name} is already in contacts.`);
+      return;
+    }
+
+    const newContact = {
+      id: nanoid(),
+      name: data.name,
+      number: data.number,
+    };
+
+    this.setState(prevState => ({
+      contacts: [newContact, ...prevState.contacts],
+    }));
+  };
+
+  changeFilter = e => {
+    this.setState({
+      filter: e.currentTarget.value,
+    });
+  };
+
+  getVisibleFilter = () => {
+    const { contacts, filter } = this.state;
+
+    const normalizeFilter = filter.toLowerCase();
+
+    return contacts.filter(todo =>
+      todo.name.toLowerCase().includes(normalizeFilter)
+    );
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  render() {
+    const filterContacts = this.getVisibleFilter();
+    const { contacts } = this.state;
+
+    return (
+      <Container>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.formSubmitHandler} />
+
+        <h2>Contacts</h2>
+        <Filter value={this.state.filter} onChange={this.changeFilter} />
+
+        {contacts.length > 0 && (
+          <ContactList
+            contacts={filterContacts}
+            onDeleteContact={this.deleteContact}
+          />
+        )}
+      </Container>
+    );
+  }
+}
